0d79a51a1923d2a20937e1a627b6ba2b10485262
Angel-Garay <a.garay8001@gmail.com>
2026-09-05 19:22:54 -0300

    Casi terminado el .sh

--- scripts/configuracion-predeterminada-switch.sh
+++ scripts/configuracion-predeterminada-switch.sh
@@ -45,67 +45,66 @@
 
 #Ahora el acceso seguro es al modo privilegiado
 #Poniendo contraseña (claro que es falsa), hay que completar todos los pasos siguientes para que recien este asegurado
-
-S1> enable
-S1# configure terminal
-S1(config)# enable password c1$c0
-S1(config)# exit
-S1#
-
-!Verificando que el acceso al modo privilegiado sea seguro
-
-S1# exit
-
-Verificacion de acceso del usuario
-Password:
-
-!Aca la primera contraseña es la que hicimos para "line con 0" = letmein
-!Luego poniendo enable se tiene que ingresar la 2da contraseña = c1$c0
-
-S1# show running-config
-
-!Hasta aca las 2 contraseñas son texto plano, se tienen que encriptar
-!Porque estoy viendo la contraseña que use para enable "modo EXEC privilegiado"
-
-!Configurando una contraseña encriptada para promocionar acceso seguro al modo privilegiado
-
-S1# config t
-S1(config)# enable secret itsasecret
-S1(config)# exit
-S1#
-
-!Verificando si la contraseña de enable secret se agrego al archivo de configuracion
-
-S1# show running-config
-
-!Ahora lo que se ve es la contraseña "secret" encriptada, las otras 2 contraseñas solo 1 se ve en texto plano, y la otra no la veo pero tampoco esta encriptada
+	S1> enable
+	S1# configure terminal
+	S1(config)# enable password c1$c0
+	S1(config)# exit
+	S1#
 
 
-!Encripte las contraseñas de consola y de enable
-
-S1# config t
-S1(config)# service password-encryption
-S1(config)# exit
-S1# show running-config
-!Aca lo que va a mostrar es que el servicio ESTA ACTIVO el de password-encryption
-!Y la contraseña "password" que se activo sin encriptacion ahora lo esta pero marca que "no se creo encriptada"
+#Verificando que el acceso al modo privilegiado sea seguro
+	S1# exit
+	Verificacion de acceso del usuario
+	Password:
 
 
-!Configurando un aviso de MOTD (mensaje del dia)
-!Cisco IOS permite configurar los mensajes que cualquier persona puede ver al iniciar sesion en el switch: MOTD
-
-S1# config t
-S1(config)# banner motd "This is a secure system. Authorized Access Only!"
-S1(config)# exit
-S1#
+#Aca la primera contraseña es la que hicimos para "line con 0" = letmein
+#Luego poniendo enable se tiene que ingresar la 2da contraseña = c1$c0
+	S1# show running-config
 
 
-!Guardando archivos de configuracion en NVRAM
-!La configuracion basica del switch se puede hacer una copia de seguridad a NVRAM.
-!Se hace para que los cambios hechos no se pierdan luego de apagar o reiniciar
+#Hasta aca las 2 contraseñas son texto plano, se tienen que encriptar
+#Porque estoy viendo la contraseña que use para enable "modo EXEC privilegiado"
 
-S1# copy running-config startup-config
-Destination filename [startup-config]?[Enter] 
-Building configuration...
-[OK]
+
+#Configurando una contraseña encriptada para promocionar acceso seguro al modo privilegiado
+	S1# config t
+	S1(config)# enable secret itsasecret
+	S1(config)# exit
+	S1#
+
+
+#Verificando si la contraseña de enable secret se agrego al archivo de configuracion
+	S1# show running-config
+
+
+#Ahora lo que se ve es la contraseña "secret" encriptada, las otras 2 contraseñas solo 1 se ve en texto plano, y la otra no la veo pero tampoco esta encriptada
+
+
+#Encripte las contraseñas de consola y de enable
+	S1# config t
+	S1(config)# service password-encryption
+	S1(config)# exit
+	S1# show running-config
+
+
+#Aca lo que va a mostrar es que el servicio ESTA ACTIVO el de password-encryption
+#Y la contraseña "password" que se activo sin encriptacion ahora lo esta pero marca que "no se creo encriptada"
+
+
+#Configurando un aviso de MOTD (mensaje del dia)
+#Cisco IOS permite configurar los mensajes que cualquier persona puede ver al iniciar sesion en el switch: MOTD
+	S1# config t
+	S1(config)# banner motd "This is a secure system. Authorized Access Only!"
+	S1(config)# exit
+	S1#
+
+
+#Guardando archivos de configuracion en NVRAM
+#La configuracion basica del switch se puede hacer una copia de seguridad a NVRAM.
+#Se hace para que los cambios hechos no se pierdan luego de apagar o reiniciar
+	S1# copy running-config startup-config
+	Destination filename [startup-config]?[Enter] 
+	Building configuration...
+	[OK]
 

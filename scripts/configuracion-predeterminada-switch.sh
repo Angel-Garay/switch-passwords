050770c384a5fd593604fa2f2e7f15f3c93bdc44
Angel-Garay <a.garay8001@gmail.com>
2026-09-05 19:17:23 -0300

    Modificando el ultimo archivo: scripts

--- scripts/configuracion-predeterminada-switch.sh
+++ scripts/configuracion-predeterminada-switch.sh
@@ -1,52 +1,50 @@
-!Ingresando al "modo EXEC privilegiado"
-
-Switch> enable
-Switch#
-
-
-!Examinar la configuracion actual del switch
-
-Switch# show running-config
-
-   Building configuration...
-
-   Current configuration : 1080 bytes
-   !
-   version 15.0
-   no service timestamps log datetime msec
-   no service timestamps debug datetime msec
-   no service password-encryption
-   !
-   hostname Switch
-   !
-   !spanning-tree mode pvst
-   spanning-tree extend system-id
-   !
-   interface FastEthernet0/1
-   !
-   interface FastEthernet0/2
+#Ingresando al "modo EXEC privilegiado"
+	Switch> enable
+	Switch#
 
 
-!Creando una configuracion basica del switch
-!Asignando un nombre al switch
+#Examinar la configuracion actual del switch
+	Switch# show running-config
 
-Switch# configure terminal
-Switch(config)# hostname S1
-S1(config)# exit
-S1#
+	   Building configuration...
 
-!Acceso seguro a la consola
-!Comentario propio: esto es para acceder a la primera interfaz CLI y la contraseña es falsa
+	   Current configuration : 1080 bytes
+  	   !
+	   version 15.0
+	   no service timestamps log datetime msec
+	   no service timestamps debug datetime msec
+  	   no service password-encryption
+   	   !
+	   hostname Switch
+	   !
+	   !spanning-tree mode pvst
+	   spanning-tree extend system-id
+	   !
+	   interface FastEthernet0/1
+	   !
+	   interface FastEthernet0/2
 
-S1# configure terminal
-S1(config)# line console 0
-S1(config-line)# password letmein
-S1(config-line)# login
-S1(config-line)# exit
-S1(config)# exit
 
-!Ahora el acceso seguro es al modo privilegiado
-!Poniendo contraseña (claro que es falsa), hay que completar todos los pasos siguientes para que recien este asegurado
+#Creando una configuracion basica del switch
+#Asignando un nombre al switch
+	Switch# configure terminal
+	Switch(config)# hostname S1
+	S1(config)# exit
+	S1#
+
+
+#Acceso seguro a la consola
+#Comentario propio: esto es para acceder a la primera interfaz CLI y la contraseña es falsa
+	S1# configure terminal
+	S1(config)# line console 0
+	S1(config-line)# password letmein
+	S1(config-line)# login
+	S1(config-line)# exit
+	S1(config)# exit
+
+
+#Ahora el acceso seguro es al modo privilegiado
+#Poniendo contraseña (claro que es falsa), hay que completar todos los pasos siguientes para que recien este asegurado
 
 S1> enable
 S1# configure terminal
